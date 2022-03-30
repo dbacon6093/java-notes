e2a157f0d07ae7f3e2091567393565d917516ef7
dbacon6093 <bacon.davidj@gmail.com>
2022-03-30 12:37:18 -0400

    The first few pages of chapter 3. Up to sesction 3.2

---
+++ chapter-three.js
@@ -0,0 +1,6 @@
+// JavaScript types can be divided into two categories: Primitive and Object types.
+// Primitive types include numbers, strings, boolean values, null and undefined.
+// Anything else is an object
+// An array is an object, as well as Set, Map, RegExp, Date, Error, functions and classes
+// Object types are mutable (changable) Primitive types are immutable.
+// JavaScript will convert values from one type to another. 
