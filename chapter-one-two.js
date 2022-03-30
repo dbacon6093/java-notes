a3ccec6573a84294add848c2b1c2c021198ece8b
dbacon6093 <bacon.davidj@gmail.com>
2022-03-30 12:20:59 -0400

    Notes for chapters 1 and 2. Comments, general syntax, identifiers, reserved words, unicode

---
+++ chapter-one-two.js
@@ -0,0 +1,19 @@
+// Single line comments look like this.
+// You need another double slash for each line.
+/* This is a multi-line comment.
+I can keep the comment going over multiple lines. */
+
+// Literals
+12 //This is the number 12
+1.2 // This is the number one point 2
+"Hello World" // This is a string
+'Hi' // Another string
+true // This is a boolean value
+false // Another boolean
+null // Absense of an object
+
+// Identifiers are used to name constants, variables, properties, functions, and classes. They must begin with a letter, _, or $.
+
+// Reserved words cannot be used as identifiers. There is a list on page 17 of the Diffinitive Book on Java.
+
+// Semi-colons are often optional at the end of a line break. But I will use them even in optional situations to keep consistency.
