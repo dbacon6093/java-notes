201af8ee338140019877788e9e669da8ca439c31
dbacon6093 <bacon.davidj@gmail.com>
2022-04-08 13:08:27 -0400

    Finished through chapter 3. I need to read this chapter again when I understand more. There is a lot here.

--- chapter-three.js
+++ chapter-three.js
@@ -48,4 +48,72 @@ s.substring(1, 4) // extracts "ell"
 s.slice (1,4) // Same as above
 s.split (", ") //split at the delimiter string
 // page 35 has a list of all different properties for strings
-// Strings a imutable. These methods create new strings
+// Strings a imutable. These methods create new strings 
+// Boolean values indicate truth or false, on/off yes/no
+// They are the result of camparisons in your code
+// && operator performs Bolean and operation
+// || indicates OR  
+// ! performs Boolean, NOT operation
+// Symbols were introduced in ES6 to serve non-string property names
+// Symbols servce as a language extension mechanism
+let s = Symbol("sym_x");
+s.toString() // => "Symbol(sym_x)"
+//Global objects is a regular JavaScript object that serves a very important purpose: the properties of this object are the globally definied indentifiers that are available to a JavaScript porgram.
+// Imutable Primitive Values and Mutable Object References:
+// Primitive values (undefinied, null, booleANS, numbers and strings)
+// Objects (arrays and functions)
+// Primitive values are immutable
+// Strings are not arrays so you cannot alter a character - instead it will provide a new string.
+let s = "hello"; // String
+s.toUpperCase(); // Returns "HELLO" but doesn't alter s
+console.log(s);
+// Objects are mutable
+let o = { x: 1 } // Start with an object
+o.x = 2; // Nutate it by changing the value of a property
+o.y = 3; // Mutate it by adding a new property
+let a = [1,2,3]; //Arrays are mutable
+a[0] = 0; //Change the value of the first element
+a[3] = 4; // Adds a new array element.
+//Objects are never equal
+// Arrays are never equal
+// Type Converstions:
+10 + "objects" // => "10 Objects" number 10 converts to string
+"7" + "4" //=> 11: Both strings convert to numbers
+// Converstions and Equality:
+// String equality operator "===" does not consider operands to be equal if they are not the same type
+// The equality operator "==" uses a flexible definition of equality
+null == undefined // True
+"0" == 0 // True
+// Explicit Converstions
+Number("3") // Number 3
+String(false) // "False" or use false.toString()
+// toFixed() converts a number with a specified number of digits after the decimal.
+let n=12345.678
+n.toFixed(0) // 12346
+n.toFixed(2) 12345.68
+//toExponential converts a number to string using exponent notation
+//toPrecision() converts a number to a string with the number of sig figs you specify.
+// 3.10 Variable Declaration and Assignment
+// ONe of the most fundamental techniques of computer programing is the use of name so or identifiers to represent values.
+// Use let and const to declare variables
+// You can declare on single lines or declare multiple variables in a single statement
+let i, sum;
+// It is good practice to assign an initial value to your variables when declaring them.
+let message = "hello";
+let i = 0, j = 0, k = 0;
+// To declare a constant  instead of a variable use const. You must initialize the constant when you declare it.
+const HO = 74;
+const C = 299792.458;
+// It is common practice to declare constants using all caps to distinguish them from variables.
+// 3.10.3 - Distructing Assignment
+let [x,y] = [1,2] // Same as let x=1, y=2
+
+
+
+
+
+
+
+
+
+
